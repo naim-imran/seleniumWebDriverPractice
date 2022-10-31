6a31cfda2101c79baad53cf6b29b6f5d271754ae
Naim Imran <naim.nrb@gmail.com>
2022-10-31 06:16:35 -0400

    AlertClass modified

--- src/test/java/webDriverPractice/test/AlertClass.java
+++ src/test/java/webDriverPractice/test/AlertClass.java
@@ -1,5 +1,20 @@
 package webDriverPractice.test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.BeforeClass;
+
+import webDriverPractice.initialization.InitialComponents;
+
 public class AlertClass {
+	WebDriver driver;
+	Actions action;
+	@BeforeClass
+	public void initialSetup() {
+		InitialComponents initialcomponent = new InitialComponents();
+		driver = initialcomponent.launchBrowser();
+		driver.get("https://www.amazon.com/");
+		 action = new Actions(driver);
+	}
 
 }
