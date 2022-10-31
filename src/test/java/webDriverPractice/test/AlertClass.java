f1fe12125c5688c014e46e8d26f684af855a78b4
Naim Imran <naim.nrb@gmail.com>
2022-10-31 07:24:10 -0400

    code added

--- src/test/java/webDriverPractice/test/AlertClass.java
+++ src/test/java/webDriverPractice/test/AlertClass.java
@@ -1,9 +1,10 @@
 package webDriverPractice.test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeClass;
-
+import org.testng.annotations.Test;
 import webDriverPractice.initialization.InitialComponents;
 
 public class AlertClass {
@@ -14,8 +15,21 @@ public class AlertClass {
 	public void initialSetup() {
 		InitialComponents initialcomponent = new InitialComponents();
 		driver = initialcomponent.launchBrowser();
-		driver.get("https://www.amazon.com/");
-		action = new Actions(driver);
+		driver.get("http://demo.guru99.com/V1/index.php");
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void alertClassTest() throws InterruptedException {
+		driver.findElement(By.xpath("//input[@name='uid']")).sendKeys("naim");
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("12345");
+		driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
+		Thread.sleep(2000L);
+
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+		Thread.sleep(5000L);
+		driver.quit();
 	}
 
 }
