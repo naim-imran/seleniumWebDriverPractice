c9cb092249ad77466bddd56d33cc789a60b4ebb5
naayeemVM <naim.nrb@gmail.com>
2023-03-08 15:14:32 -0500

    R0008TakeScreenShots added

--- src/test/java/webDriverPractice/test/R0009Synchronization.java
+++ src/test/java/webDriverPractice/test/R0009Synchronization.java
@@ -1,5 +1,30 @@
 package webDriverPractice.test;
 
-public class R0009Synchronization {
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
+import webDriverPractice.initialization.InitialComponents;
+
+public class R0009Synchronization {
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setupInitialComponents() throws InterruptedException {
+		InitialComponents initialComponents = new InitialComponents();
+		driver = initialComponents.launchBrowser();
+		driver.get("https://www.ebay.com/");
+		
+		Thread.sleep(5);
+	}
+	
+	@AfterMethod
+	public void quitBrowser() {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		driver.quit();
+	}
 }
