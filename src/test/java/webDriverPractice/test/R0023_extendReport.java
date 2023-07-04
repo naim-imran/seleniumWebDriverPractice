6cc895ab40ebee96ab80ba3e4e89d7dd51c65da6
naim <naim.nrb@gmail.com>
2023-07-04 01:38:21 -0400

    R0023_extendReport.java class modified

--- src/test/java/webDriverPractice/test/R0023_extendReport.java
+++ src/test/java/webDriverPractice/test/R0023_extendReport.java
@@ -16,29 +16,30 @@ public class R0023_extendReport {
 		ExtentSparkReporter reporter = new ExtentSparkReporter(System.getProperty("user.dir")+ "/src/test/java/extentReports/"+reportTime+"index.html");
 		reporter.config().setReportName("Extent report practice");
 		reporter.config().setDocumentTitle("Extent report");
-		
+	
 		logReport = new ExtentReports();
 		logReport.attachReporter(reporter);
 		logReport.setSystemInfo("Tester", "Naayeem");
 		
+		
 	}
 	
-	@Test
+	@Test(priority = 2)
 	public void testExtentReport() {
 		logReport.createTest("testing extent report");
-		System.out.println("test");
+		System.out.println("test 01");
 	}
 	
-	@Test
+	@Test(priority = 1)
 	public void testExtentReport2() {
 		logReport.createTest("testing extent report 02");
-		System.out.println("test");
+		System.out.println("test 02");
 	}
 	
-	@Test
+	@Test(priority = 0)
 	public void testExtentReport03() {
 		logReport.createTest("testing extent report03");
-		System.out.println("test");
+		System.out.println("test 03");
 	}
 	
 	@AfterTest
